40146a2a5a1e51754be6d482e0d4e30d4c0e0099
Martin Karlsson <klickas@live.se>
2019-02-14 13:15:04 +0100

    First commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
+import reducers from "./reducers";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+registerServiceWorker();
