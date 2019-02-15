951f8000be826ae38864fe2384a69ca0113cc9d0
Tobias Rosengren <tobias.filip.rosengren@gmail,com>
2019-02-15 14:28:52 +0100

    Oscar fixed the main css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 import SideNavBar from "./components/navbar/SideNavBar";
+import Schedule from "./components/schedule/Schedule";
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <SideNavBar />
+      <div style={{height: "100%", display:"flex"}}>
+        <div>
+          <SideNavBar />
+        </div>
+        <div style={{width: "100%"}}>
+          <Schedule/>
+        </div>
       </div>
     );
   }
