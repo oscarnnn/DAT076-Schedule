517c9e8747d4d9592e6affeeaf05bfc631b8127f
Tobias Rosengren <tobias.filip.rosengren@gmail,com>
2019-02-27 13:30:14 +0100

    added members page

--- src/components/members/Members.js
+++ src/components/members/Members.js
@@ -1,71 +1,54 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { Redirect } from 'react-router-dom'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { compose } from "redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { Redirect } from "react-router-dom"
+
 
 class Members extends Component {
 
-    render() {
-        const { auth } = this.props;
-        if (!auth.uid) return <Redirect to='/signin' />
-        return(
-          
-        <body>
-          <h1>Example shit, swap out for user data from collection</h1>
-                  <table class="highlight centered">
-        <thead>
-          <tr>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Phone</th>
-          </tr>
-        </thead>
+  render () {
+    let display = this.props.users;
 
-        <tbody>
-          <tr>
-            <td>Toby</td>
-            <td>Toby@Doby.com</td>
-            <td>911</td>
-          </tr>
-          <tr>
-            <td>Maka</td>
-            <td>Maka@kaka.com</td>
-            <td>112</td>
-          </tr>
-          <tr>
-            <td>OsKar</td>
-            <td>Oskar@OskarmedK.com</td>
-            <td>123</td>
-          </tr>
-        </tbody>
-      </table>
-        </body>
-      )
+    if (!this.props.auth.uid) {
+      return <Redirect to="/signin" />
     }
-  
-
+    return (
+      <div>
+        <table className="centered highlight">
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+            </tr>
+          </thead>
+          <tbody>
+              {display.map(item => <tr key={item.id}>
+                                    <td>{item.firstName}</td>
+                                    <td>{item.lastName}</td>
+                                   </tr>)}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
 
-const mapStateToProps = (state) => {
-    // console.log(state);
+const mapStateToProps = state => {
+  if (state.firestore.ordered.users) {
     return {
-      auth: state.firebase.auth,
-    }
+      users: state.firestore.ordered.users,
+      auth: state.firebase.auth
+    };
+  } else {
+    return {
+      users: [],
+      auth: state.firebase.auth
+    };
   }
-  
-  export default connect(mapStateToProps)(Members)
+};
 
-
-/*const mapStateToProps = (state) => {
-  console.log(state);
-  return {
-    users: state.firestore.ordered.users
-  }
-}*/
-
-  
- /* export default compose(
-    connect(mapStateToProps),
-    firestoreConnect([
-      {collection: 'users' }
-    ])
-  )(Members)*/
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([{ collection: "users" }])
+)(Members);
