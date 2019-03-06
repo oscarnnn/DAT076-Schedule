11eef4bf30a1702eeaf06997676f2b9c1d65feef
oscarnnn <35729517+oscarnnn@users.noreply.github.com>
2019-03-06 12:16:58 +0100

    added commented code

--- src/store/actions/adminActions.js
+++ src/store/actions/adminActions.js
@@ -1,3 +1,6 @@
+//This actioncreator will first try to add the given email to the given organization in the database,
+//if it was succeeded the function will dispatch another action with typ "VALID_MEMBER".
+//If the email wasn't found in the database a action with typ "MEMBER_DOES_NOT_EXIST" will be dispatched
 export const isValidEmail = ( email, org ) => {
   return (dispatch, getState, {getFirebase, getFirestore}) => {
     const firestore = getFirestore();
@@ -21,6 +24,8 @@ export const isValidEmail = ( email, org ) => {
   }
 }
 
+//This actioncreator will first try to add the given organization to the given userid. If successful it will dispatch
+//an action with type "ADD_ORGANIZATION", else "ADD_ORGANIZATION_ERROR"
 export const addOrganization = (organization, userid) => {
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     const firestore = getFirestore();
